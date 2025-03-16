790fbbdba43cb4612fe7cece8403ef5b30d3963c
Ali <alioct825@gmail.com>
2025-03-16 23:49:27 +0500

    last step

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Ali! I did it at last!"
+    return "Hello, Ali! This is the last step!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
