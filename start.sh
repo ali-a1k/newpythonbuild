25a42df8b4474b904de1a2c267de27ee982b0cf1
Ali <alioct825@gmail.com>
2025-03-16 22:56:03 +0500

    new build

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu
-pip3 install -r requirements.txt
-nohup python3 app.py > app.log 2>&1 &
+source venv/bin/activate
+pip install -r requirements.txt
+sudo systemctl restart nginx
